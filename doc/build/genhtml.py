43fd83e745da6125c918f861422c6a50866d218a
Mike Bayer <mike_mp@zzzcomputing.com>
2007-06-13 02:13:27 +0000

    0.1.7

--- doc/build/genhtml.py
+++ doc/build/genhtml.py
@@ -24,7 +24,7 @@ files = [
     ]
 
 title='Mako Documentation'
-version = '0.1.6'
+version = '0.1.7'
 
 root = toc.TOCElement('', 'root', '', version=version, doctitle=title)
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.1.6'
+version = '0.1.7'
 
 setup(name='Mako',
       version=version,
