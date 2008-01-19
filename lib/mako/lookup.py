08b7b7cb9ecda31de2f6652a704f80a706c87891
Mike Bayer <mike_mp@zzzcomputing.com>
2008-01-19 23:13:34 +0000

    copyright !

--- lib/mako/lookup.py
+++ lib/mako/lookup.py
@@ -1,5 +1,5 @@
 # lookup.py
-# Copyright (C) 2006, 2007 Michael Bayer mike_mp@zzzcomputing.com
+# Copyright (C) 2006, 2007, 2008 Michael Bayer mike_mp@zzzcomputing.com
 #
 # This module is part of Mako and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
